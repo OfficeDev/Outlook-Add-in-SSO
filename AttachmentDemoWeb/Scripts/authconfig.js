e77831b4edc2590cf545582eb0dd79007486fa2d
Jason Johnston <jasonjoh@users.noreply.github.com>
2017-05-31 12:45:02 -0400

    Added copyright to authconfig

--- AttachmentDemoWeb/Scripts/authconfig.js
+++ AttachmentDemoWeb/Scripts/authconfig.js
@@ -1,4 +1,5 @@
-﻿var authConfig = {
+﻿// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE.txt in the project root for license information.
+var authConfig = {
     clientId: "YOUR APP ID HERE",
     scopes: "Files.ReadWrite",
     redirectUrl: "https://localhost:44349/MessageRead.html"
