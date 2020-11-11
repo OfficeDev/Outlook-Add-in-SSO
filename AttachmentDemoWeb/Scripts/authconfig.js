ea922656ad88ededc8768b26e45a3125c1a16a19
rykoma <rykoma@microsoft.com>
2020-11-11 10:13:31 +0900

    Update authconfig.js
    Remove sample client id.

--- AttachmentDemoWeb/Scripts/authconfig.js
+++ AttachmentDemoWeb/Scripts/authconfig.js
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE.txt in the project root for license information.
 var authConfig = {
-    clientId: "b93c9c2f-f348-442c-83ee-6a2c859ff9d6",
+    clientId: "Your APP ID Here",
     scopes: "Files.ReadWrite",
     redirectUrl: "https://localhost:44349/MessageRead.html"
 };
